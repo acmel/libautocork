e9c535851decc86a23fdb7dc4a43f0a8e2859197
Arnaldo Carvalho de Melo <acme@redhat.com>
2007-11-26 20:42:08 -0200

    [LIBAUTOCORK]: Its a library, so license it under the LGPL
    Thanks to Aristeu Rozanski for bringing up this issue.
    
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- libautocork.c
+++ libautocork.c
@@ -2,7 +2,7 @@
   Copyright (C) 2007 Arnaldo Carvalho de Melo <acme@redhat.com>
 
   This program is free software; you can redistribute it and/or modify it
-  under the terms of version 2 of the GNU General Public License as
+  under the terms of version 2.1 of the GNU Lesser General Public License as
   published by the Free Software Foundation.
 */
 
